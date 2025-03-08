68945c72d89e94331e609230173bd9498b5f1f71
sehyphen <tasnemtrabeh@gmail.com>
2025-03-08 17:44:45 +0200

    USER.NAME"TASNEM"

--- movie.js
+++ movie.js
@@ -85,4 +85,7 @@ app.get('/', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.get('/test', (req, res) => {
+    res.send('Server is working!');
+  });
