a37038179212768049ee366e7c844953f34966f4
sehyphen <tasnemtrabeh@gmail.com>
2025-03-08 15:24:25 +0200

    user.name"tasnem"

--- movie.js
+++ movie.js
@@ -1,56 +1,88 @@
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
-
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
 
-// הגדרת EJS כמנוע תבניות
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/movies', express.static('movies'));
 
-// הגדרת תיקיות סטטיות
-app.use(express.static("public"));
+const db = new sqlite3.Database('rtfilms.db');
 
-// חיבור למסד הנתונים SQLite
-const db = new sqlite3.Database("./db/rtfilms.db", sqlite3.OPEN_READONLY, (err) => {
-    if (err) {
-        console.error("Error opening database:", err.message);
-    } else {
-        console.log("Connected to the SQLite database.");
-    }
-});
-
-// נתיב ראשי שמציג סרט לפי שאילתת URL
-app.get("/", (req, res) => {
-    const movieTitle = req.query.title;
-    if (!movieTitle) {
-        return res.send("Missing movie title parameter");
-    }
-
-    // שליפת נתוני הסרט מהמסד
-    const query = `SELECT * FROM movies WHERE code = ?`;
-    db.get(query, [movieTitle], (err, movie) => {
-        if (err) {
-            return res.status(500).send("Database error");
-        }
-        if (!movie) {
-            return res.status(404).send("Movie not found");
-        }
-
-        // שליפת הביקורות
-        const reviewQuery = `SELECT * FROM reviews WHERE movie_code = ?`;
-        db.all(reviewQuery, [movieTitle], (err, reviews) => {
-            if (err) {
-                return res.status(500).send("Database error");
-            }
-            
-            res.render("movie", { movie, reviews });
+// Promisify database functions
+const getFilm = (title) => {
+    return new Promise((resolve, reject) => {
+        db.get(`
+            SELECT FilmCode, Title, Year, Score 
+            FROM Films 
+            WHERE Title = ?
+        `, [title], (err, film) => {
+            if (err || !film) reject('Movie not found');
+            resolve(film);
         });
     });
+};
+
+const getFilmDetails = (code) => {
+    return new Promise((resolve, reject) => {
+        db.all(`
+            SELECT Attribute, Value 
+            FROM FilmDetails 
+            WHERE FilmCode = ?
+        `, [code], (err, details) => {
+            if (err) reject(err);
+            resolve(details);
+        });
+    });
+};
+
+const getReviews = (code) => {
+    return new Promise((resolve, reject) => {
+        db.all(`
+            SELECT ReviewerName, Affiliation, ReviewText, Rating 
+            FROM Reviews 
+            WHERE FilmCode = ?
+        `, [code], (err, reviews) => {
+            if (err) reject(err);
+            resolve(reviews);
+        });
+    });
+};
+
+app.get('/', async (req, res) => {
+    try {
+        const title = req.query.title;
+        if (!title) return res.status(400).send('Missing title parameter');
+
+        const film = await getFilm(title);
+        const details = await getFilmDetails(film.FilmCode);
+        const reviews = await getReviews(film.FilmCode);
+
+        const movie = {
+            code: film.FilmCode,
+            name: film.Title,
+            year: film.Year,
+            rating: film.Score,
+            genre: '',
+            runtime: '',
+            starring: ''
+        };
+
+        details.forEach(({Attribute, Value}) => {
+            movie[Attribute.toLowerCase()] = Value;
+        });
+
+        const formattedReviews = reviews.map(review => ({
+            critic_name: review.ReviewerName,
+            publication: review.Affiliation,
+            text: review.ReviewText,
+            rating: review.Rating >= 60 ? 'FRESH' : 'ROTTEN'
+        }));
+
+        res.render('movie', { movie, reviews: formattedReviews });
+    } catch (error) {
+        res.status(404).send(error);
+    }
 });
 
-// הפעלת השרת
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
